867ee82f5574f750bcb5ef9f65a87a6a57acbab9
Larry <larryle7680@gmail.com>
2026-05-08 07:46:11 -0500

    Started over. Prompt users for their names and stored it. Created a deck. Shuffled the deck.

--- src/main/java/com/pluralsight/BlackJackApp.java
+++ src/main/java/com/pluralsight/BlackJackApp.java
@@ -1,30 +1,26 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class BlackJackApp {
+    static Scanner theScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-     //Create a deck
+        //Prompt users for their names and store it
+        System.out.println("=== Black Jack ===");
+        System.out.println();
+        System.out.print("Player 1 Name: ");
+        String hand1 = theScanner.nextLine();
+        System.out.println();
+        System.out.print("Player 2 Name: ");
+        String hand2 = theScanner.nextLine();
+        System.out.println();
+
+        //Creating a deck
         Deck deck = new Deck();
 
-        //Create the players
-        Hand hand1 = new Hand();
-        Hand hand2 = new Hand();
+        //Shuffle the deck
+        deck.shuffle();
 
-        //Deal 2 cards to each player
-        for(int i = 0; i <= 1; i++){
-            //Shuffle the deck
-            deck.shuffle();
-            //Grab a card from the deck to hand1
-            Card card1 = deck.deal();
-            //Now add it to hand1
-            hand1.deal(card1);
-            //Grab a card from the deck to hand2
-            Card card2 = deck.deal();
-            //Now add it to hand2
-            hand2.deal(card2);
 
-            System.out.println("Hand 1 Cards: " + card1.getPointValue());
-            System.out.println("Hand 2 Cards : " + card2.getPointValue());
-
-        }
     }
 }
